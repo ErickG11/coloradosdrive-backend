6d3f7a386679ec40697b5536012bf108b9e79e1e
ErickG11 <grandaerick8@gmail.com>
2026-09-02 15:08:07 -0500

    chore: configurar eslint flat config y prettier
    Usa el flat config de ESLint 9 (eslint.config.js) con los conjuntos de reglas strictTypeChecked + stylisticTypeChecked de typescript-eslint, apoyado en un tsconfig.eslint.json dedicado (incluye src y tests, ya que el tsconfig de build excluye tests). Prettier se mantiene separado de ESLint (el formato se resuelve via eslint-config-prettier, que desactiva las reglas de estilo en conflicto) para lints mas rapidos. Agrega package-lock.json para instalaciones reproducibles.

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const globals = require('globals');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['tests/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+  eslintConfigPrettier,
+);
